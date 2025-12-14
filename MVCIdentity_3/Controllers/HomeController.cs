94d878aa67d6e7b454f5f66b989311b7c9ec68e5
cagri.yolyapar <cagri.yolyapar@gmail.com>
2025-12-14 11:33:36 +0300

    Home Controller sign in düzenlemesi yapıldı

--- MVCIdentity_3/Controllers/HomeController.cs
+++ MVCIdentity_3/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MVCIdentity_3.Models.Entities;
 using MVCIdentity_3.Models.ViewModels.AppUsers.RequestModels;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace MVCIdentity_3.Controllers
 {
@@ -101,6 +102,56 @@ namespace MVCIdentity_3.Controllers
             };
 
             return View(usModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInRequestModel model)
+        {
+            ValidationResult validationResult = _userSignInRequestValidator.Validate(model);
+            if (validationResult.IsValid)
+            {
+                AppUser appUser = await _userManager.FindByNameAsync(model.UserName);
+                if(appUser == null)
+                {
+                    TempData["message"] = "Kullanıcı bulunamadı";
+                    return RedirectToAction("SignIn", new { returnUrl = model.ReturnUrl });
+                }
+
+                SignInResult signInResult = await _signInManager.PasswordSignInAsync(appUser, model.Password, model.RememberMe, true);
+                if (signInResult.Succeeded) 
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+
+                    IList<string> userRoles = await _userManager.GetRolesAsync(appUser);
+                    if (userRoles.Contains("Admin")) return RedirectToAction("AdminPanel", "Auth");
+                    else if (userRoles.Contains("Member")) return RedirectToAction("MemberPanel");
+                    return RedirectToAction("Panel");
+                }
+                else if (signInResult.IsLockedOut)
+                {
+                    DateTimeOffset? lockOutEndDate = await _userManager.GetLockoutEndDateAsync(appUser);
+                    ModelState.AddModelError("", $"Hesabınız {(lockOutEndDate.Value.UtcDateTime - DateTimeOffset.UtcNow).Minutes} dakika süreyle askıya alınmıstır");
+
+                }
+                else
+                {
+                    int maxFailedAttempts = _userManager.Options.Lockout.MaxFailedAccessAttempts; //kullanıcının kac kez yanlıs giriş yapabilecegini söyler
+                    string message = $"Eger {maxFailedAttempts - await _userManager.GetAccessFailedCountAsync(appUser)} kez daha yanlıs yaparsanız hesabınız gecici olarak askıya alınacaktır";
+                    ModelState.AddModelError("", message);
+                }
+            }
+            else
+            {
+                foreach(ValidationFailure validationFailure in validationResult.Errors)
+                {
+                    ModelState.AddModelError(validationFailure.PropertyName, validationFailure.ErrorMessage);
+                }
+            }
+
+            return View(model);
         }
     }
 }
